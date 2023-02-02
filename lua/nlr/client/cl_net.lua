653ffff18613790b9aeb05652cf14addd0e0e717
flyingkarii <123355664+flyingkarii@users.noreply.github.com>
2023-02-02 00:41:04 -0400

    Delete lua directory

--- lua/nlr/client/cl_net.lua
+++
@@ -1,8 +0,0 @@
-net.Receive("knlr_send", function(len, ply)
-    local color1 = net.ReadColor()
-    local text1 = net.ReadString()
-    local color2 = net.ReadColor()
-    local text2 = net.ReadString()
-
-    chat.AddText(color1, text1, color2 or Color(255, 255, 255), " " .. text2 or "")
-end)
