cf4b2adf00ff8b1978f8af62e1f8af8e321fec4d
flyingkarii <123355664+flyingkarii@users.noreply.github.com>
2023-02-02 00:41:37 -0400

    Delete kari_afk/lua directory

--- kari_afk/lua/afk/client/cl_chat.lua
+++
@@ -1,8 +0,0 @@
-net.Receive("kari_afk_msg", function(len)
-    local color1 = net.ReadColor()
-    local text1 = net.ReadString()
-    local color2 = net.ReadColor()
-    local text2 = net.ReadString()
-    
-    chat.AddText(color1, text1, color2 or color_white, text2 or "")
-end)
--- kari_afk/lua/afk/server/sv_afk.lua
+++
@@ -1,7 +0,0 @@
-hook.Add("PlayerInitialSpawn", "kari_afk_init", function(ply, transition)
-    ply:InitAfk()
-end)
-
-hook.Add("PlayerDisconnected", "kari_afk_disconnected", function(ply)
-    timer.Remove("afk_timer_" .. ply:SteamID64())
-end)
